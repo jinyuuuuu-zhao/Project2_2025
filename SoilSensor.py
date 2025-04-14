beed19f05fa3d8ab5e59569c252b2ba0446d83d0
jinyuuuuu-zhao <2577839182@qq.com>
2025-04-14 14:22:50 +0800

    Initial commit of SoilSensor.py

---
+++ SoilSensor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+#GPIO SETUP
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel,GPIO.IN)
+def callback(channel):
+    if GPIO.input(channel):
+        print("Water Detected!")
+    else:
+        print("Water Detected!")
+GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
+GPIO.add_event_callback(channel,callback)
+#infinite loop
+while True:
+    time.sleep(1)
